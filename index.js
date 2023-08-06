0430c440ec4f28028ad28864b0b806b7f8df8bef
Inna <121440205+Inna-html@users.noreply.github.com>
2023-08-06 19:51:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const saveTodos = () => {
 
 const loadTodos = () => {
     todosElement.innerHTML = '';
-    const list = JSON.parse(localStorage.getItem('todos') || '');
+    const list = JSON.parse(localStorage.getItem('todos'));
     for (const {done, text} of list) {
         createTodo(text, done);
     }
